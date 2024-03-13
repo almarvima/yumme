81457dd07c64e683b4758bb71d02e565ed2974ba
ALBERT\Albert <albert.figuerola.gmz@gmail.com>
2024-03-13 21:03:13 +0100

    - Added: Role entity - Added: User entity

---
+++ backend-yumme/src/main/java/com/yumme/backendyumme/domain/Role.java
@@ -0,0 +1,42 @@
+package com.yumme.backendyumme.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    @OneToOne(mappedBy = "role")
+    private User user;
+
+    public Role() {
+    }
+
+    public Role(Long id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
--- backend-yumme/src/main/java/com/yumme/backendyumme/domain/User.java
+++ backend-yumme/src/main/java/com/yumme/backendyumme/domain/User.java
@@ -1,4 +1,28 @@
 package com.yumme.backendyumme.domain;
 
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String last_name;
+
+    private String email;
+
+    private String password;
+
+    private String status;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+
 }
