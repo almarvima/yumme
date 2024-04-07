3c9a03178eb59b5acbe50a54decfb9df749b1164
Manuel Garcia-Nieto <almarvima.yumme@gmail.com>
2024-04-07 23:49:30 +0200

    Add new avocado images for errors 403, 404 and 500

--- Frontend - Yumme/src/components/ErrorPages/Forbidden.jsx
+++ Frontend - Yumme/src/components/ErrorPages/Forbidden.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import myImage from '../../images/forbidden.png'
+import myImage from '../../images/403.png'
 
 const Forbidden = () => {
   return (
     <div className='flex flex-col items-center justify-center my-4 p-4 md:pt-10'>
       <h2 className='text-6xl font-bold mb-6'>Forbidden</h2>
       <p className='mb-6 text-lg'>Oh oh! You can't access this page.</p>
-      <img className='w-3/4 max-w-4xl mx-auto shadow-2xl' src={myImage} alt='Forbidden access' />
+      <img className='w-3/4 max-w-2xl mx-auto shadow-2xl' src={myImage} alt='Forbidden access' />
     </div>
   )
 }
